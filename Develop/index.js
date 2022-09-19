8f1b632c7c1faabcb61e0dfc288966a52feb817e
unknown <hashmi.pgh@gmail.com>
2022-09-19 11:28:12 -0400

    commit to code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,79 @@
-// TODO: Include packages needed for this application
+// required node packages
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+// array of questions prompted to the user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project/ repository?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a breif description of your project'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'List any installations used or needed for this project/ repo.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'List the usage used or needed for this project/ repo.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'List any test cases or testing for this project/ repo.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'List any contributors or contributions for this project/ repo.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What would you like to use for your license?',
+        choices: [ 'Apache2.0', 'GPL3.0', 'MIT', 'None' ]
+    },
+];
+
+const userQuestions = function() {
+    return inquirer.prompt(questions)
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile() {
+    fs.writeFile('README.md', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been successfully created!")
+        
+}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+function init() {
+    userQuestions().then(() => writeToFile())
+}
+
+
+init()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'none')
+    return ""
+  else if(license === 'MIT')
+    return `![license badge](https://img.shields.io/badge/License-MIT-brightgreen)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
